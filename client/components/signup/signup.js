bc54fcf16d1e98a41f0e5eb597b27d3713d79da1
Akshay-agarwal <akshay.agarwal01@sjsu.edu>
2017-10-23 12:56:32 -0700

    add sign up component

---
+++ client/components/signup/signup.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class signup extends React.Component {
+    render() {
+        return (
+            <h1>Sign up Page</h1>
+        );
+    }
+}
+
+export default signup;
